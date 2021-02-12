b64a11edc4f913230b5c5d67474b1e852ef50bdb
limelier <tudor.iacobescu.1@gmail.com>
2021-02-12 20:04:46 +0200

    Add crossover operator

--- src/genetic/definitions.rs
+++ src/genetic/definitions.rs
@@ -3,7 +3,7 @@ pub(crate) const SCORE_ABSENT_AIR: f64 = -0.5;
 pub(crate) const SCORE_PRESENT_BLOCK: f64 = 1.0;
 pub(crate) const SCORE_ABSENT_BLOCK: f64 = 0.0;
 
-const INDIVIDUAL_SIZE: usize = 256;
+pub(crate) const INDIVIDUAL_SIZE: usize = 256;
 pub type Individual = [u8; INDIVIDUAL_SIZE];
 
 pub(crate) const MUTATION_CHANCE: f64 = 0.05;
--- src/genetic/operators.rs
+++ src/genetic/operators.rs
@@ -1,19 +1,47 @@
-use crate::genetic::definitions::{Individual, MUTATION_CHANCE};
+use crate::genetic::definitions::{Individual, MUTATION_CHANCE, INDIVIDUAL_SIZE};
 use rand::Rng;
 
-pub(crate) fn mutate(individual: &Individual) -> Individual {
+pub(crate) fn mutate(individual: &mut Individual) {
     let mut rng = rand::thread_rng();
-    let mut modified = [0; 256];
 
-    for (i, byte) in individual.iter().enumerate() {
+    for byte in individual {
         let roll: f64 = rng.gen();
 
-        modified[i] = if roll <= MUTATION_CHANCE {
+        *byte = if roll <= MUTATION_CHANCE {
             rng.gen_range(0..=255)
         } else {
             *byte
         }
     }
+}
 
-    modified
+pub(crate) fn crossover(first: &mut Individual, second: &mut Individual) {
+    let mut rng = rand::thread_rng();
+    let cut_point = rng.gen_range(1..INDIVIDUAL_SIZE - 1);
+    let swap_before: bool = rng.gen();
+
+    let swap_range = if swap_before {
+        0..cut_point
+    } else {
+        cut_point..INDIVIDUAL_SIZE
+    };
+
+    for i in swap_range {
+        let aux = first[i];
+        first[i] = second[i];
+        second[i] = aux;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn crossover_test() {
+        let mut first = [0; 256];
+        let mut second = [1; 256];
+        crossover(&mut first, &mut second);
+        assert_ne!(first[0], first[255]);
+    }
 }
