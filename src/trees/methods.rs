a5b9ed32faea2d5e7b0a6df8242b1d6904c6a887
limelier <tudor.iacobescu.1@gmail.com>
2021-04-07 18:56:51 +0300

    Simplified nth node searching, and removed useless functions

--- src/trees/methods.rs
+++ src/trees/methods.rs
@@ -30,6 +30,27 @@ impl Node {
         }
     }
 
+    /// Get a vector of references to all of the node's children
+    pub fn children_mut(&mut self) -> Vec<&mut Node> {
+        match self {
+            Print(c1) |
+            Unary(_, c1) |
+            Store(_, c1)
+            => vec!(c1),
+
+            Binary(_, c1, c2) |
+            While(c1, c2) |
+            Compare(c1, c2) |
+            Then(c1, c2)
+            => vec!(c1, c2),
+
+            If(c1, c2, c3)
+            => vec!(c1, c2, c3),
+
+            _ => vec!(),
+        }
+    }
+
     /// Iterate through all nodes of tree, in preorder, yielding references
     pub fn nodes(&self) -> impl Iterator<Item = &Node> {
         let mut queue: VecDeque<&Node> = VecDeque::new();
@@ -42,95 +63,39 @@ impl Node {
         })
     }
 
-    /// Search for the nth node in a tree, in preorder.
-    ///
-    /// Return either:
-    ///     - the count of the nodes in the tree, if the tree does not have an nth node
-    ///     - a list of directions (which child next?) to get to the node from the root
-    fn search_nth_node(&self, n: usize) -> SearchResult {
-        if n == 0 {
-            return SearchResult::Directions(vec!());
+    /// Get a mutable reference to the nth node of a tree, in preorder
+    pub(crate) fn get_nth_node_mut(&mut self, n: usize) -> Option<&mut Node> {
+        match self.search_nth_node_mut(n) {
+            SearchResult::Count(_) => None,
+            SearchResult::Reference(reference) => Some(reference),
         }
+    }
 
+    /// Search for the nth child (in preorder) of a tree, returning:
+    /// - a mutable reference to the node, if the node was found
+    /// - the count of the tree's nodes, otherwise
+    fn search_nth_node_mut(&mut self, n: usize) -> SearchResult {
+        if n == 0 {
+            return SearchResult::Reference(self);
+        }
         let mut checked_nodes = 1;
-        for (idx, child) in self.children().iter().enumerate() {
-            let res = child.search_nth_node(n - checked_nodes);
+        for child in self.children_mut() {
+            let res = (*child).search_nth_node_mut(n - checked_nodes);
 
             match res {
                 SearchResult::Count(count) => { checked_nodes += count; }
-                SearchResult::Directions(dirs) => {
-                    let mut full_dirs = vec!(idx as u8);
-                    full_dirs.append(&mut dirs.clone());
-                    return SearchResult::Directions(full_dirs);
-                }
+                SearchResult::Reference(_) => { return res; }
             }
         }
 
         SearchResult::Count(checked_nodes)
     }
-
-    /// Get a mutable reference to the nth child of a node, if it exists
-    fn get_nth_child_mut(&mut self, n: u8) -> Option<&mut Node> {
-        match self {
-            Print(c1) |
-            Unary(_, c1) |
-            Store(_, c1) => {
-                match n {
-                    0 => Some(c1),
-                    _ => None,
-                }
-            },
-
-            Binary(_, c1, c2) |
-            While(c1, c2) |
-            Compare(c1, c2) |
-            Then(c1, c2) => {
-                match n {
-                    0 => Some(c1),
-                    1 => Some(c2),
-                    _ => None,
-                }
-            },
-
-            If(c1, c2, c3) => {
-                match n {
-                    0 => Some(c1),
-                    1 => Some(c2),
-                    2 => Some(c3),
-                    _ => None,
-                }
-            },
-
-            _ => None,
-        }
-    }
-
-    /// Get an immutable reference to the nth node of a tree, in preorder
-    pub(crate) fn get_nth_node_mut(&mut self, n: usize) -> Option<&mut Node> {
-        let result = self.search_nth_node(n);
-
-        match result {
-            SearchResult::Directions(vec) => {
-                let mut reference = self;
-                for idx in vec {
-                    reference = reference.get_nth_child_mut(idx).unwrap();
-                }
-                Some(reference)
-            }
-            _ => None,
-        }
-    }
-
-    /// Get the number of nodes in the tree
-    pub(crate) fn node_count(&self) -> usize {
-        1 + self.children().iter().map(|child| child.node_count() ).sum::<usize>()
-    }
 }
 
 #[derive(Debug)]
-enum SearchResult {
+enum SearchResult<'a> {
     Count(usize),
-    Directions(Vec<u8>),
+    Reference(&'a mut Node)
 }
 
 #[cfg(test)]
@@ -203,56 +168,11 @@ mod tests {
     }
 
     #[test]
-    fn test_search_nth_node_directions() {
-        let tree = big_tree();
-
-        let result = tree.search_nth_node(3);
-        if let SearchResult::Directions(vec) = result {
-            assert_eq!(vec, [0, 1]);
-            return;
-        }
-        panic!("did not find node #3");
-    }
-
-    #[test]
-    fn test_search_nth_node_count() {
-        let tree = big_tree();
-
-        let result = tree.search_nth_node(10);
-        if let SearchResult::Count(n) = result {
-            assert_eq!(n, 7);
-            return;
-        }
-        panic!("returned directions instead");
-    }
-
-    #[test]
-    fn get_nth_child_mut_hit() {
-        let mut tree = Print(Box::from(Val(Source::Value(1))));
-
-        let child = tree.get_nth_child_mut(0);
-        if let Some(&mut Val(Source::Value(1))) = child {
-            return;
-        }
-        panic!("could not find child");
-    }
-
-    #[test]
-    fn get_nth_child_mut_miss() {
-        let mut tree = Print(Box::from(Val(Source::Value(1))));
-
-        let child = tree.get_nth_child_mut(1);
-        if let None = child {
-            return;
-        }
-        panic!("found a child where none should have been found");
-    }
-
-    #[test]
     fn test_get_nth_node_mut_hit() {
         let mut tree = big_tree();
 
         let result = tree.get_nth_node_mut(3);
+        dbg!(&result);
         if let Some(&mut Val(Source::Value(2))) = result {
             return;
         }
