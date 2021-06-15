e42c9098a9570d87826fa47b6d15b3a7292af6c6
limelier <tudor.iacobescu.1@gmail.com>
2021-06-15 15:04:56 +0300

    Reorganize definitions file.

--- src/genetic/definitions.rs
+++ src/genetic/definitions.rs
@@ -33,30 +33,33 @@ pub(crate) enum SelectionMethod {
     Tournament,
 }
 
+// Selection
 pub(crate) const SELECTION_METHOD: SelectionMethod = SelectionMethod::Tournament;
-
-pub(crate) const INDIVIDUALS_PER_METHOD_AND_DEPTH: usize = 5;
-pub(crate) const MIN_GEN_DEPTH: usize = 2;
-pub(crate) const MAX_GEN_DEPTH: usize = 6;
-pub(crate) const POPULATION_SIZE: usize = (MAX_GEN_DEPTH - MIN_GEN_DEPTH + 1) * INDIVIDUALS_PER_METHOD_AND_DEPTH * 2;
-pub(crate) const MAX_PROGRAM_RUNTIME_MS: u128 = 10;
-pub(crate) const GEN_COUNT: usize = 1000;
-pub(crate) const CROSSOVER_SIZE: usize = (POPULATION_SIZE as f64 * 0.2) as usize;
-
-pub(crate) const MAX_DEPTH: usize = 12;
-/// The bigger, the less important tree depth is in individual evaluation
-pub(crate) const DEPTH_SOFTENER: f64 = 6.0;
-pub(crate) const SCORE_PROGRAM_ERROR: f64 = f64::NEG_INFINITY;
-pub(crate) const SCORE_DEPTH_LIMIT_EXCEEDED: f64 = f64::NEG_INFINITY;
-
 // TOURNAMENT ONLY
 pub(crate) const TOURNAMENT_SIZE: usize = 4;
 pub(crate) const TOURNAMENT_P: f64 = 1.0;
 // FITNESS-WEIGHTED ONLY
 pub(crate) const SELECTION_PRESSURE: f64 = 1.0;
 
+// Generation
+pub(crate) const INDIVIDUALS_PER_METHOD_AND_DEPTH: usize = 5;
+pub(crate) const MIN_GEN_DEPTH: usize = 2;
+pub(crate) const MAX_GEN_DEPTH: usize = 6;
+pub(crate) const POPULATION_SIZE: usize = (MAX_GEN_DEPTH - MIN_GEN_DEPTH + 1) * INDIVIDUALS_PER_METHOD_AND_DEPTH * 2;
+pub(crate) const GEN_COUNT: usize = 1000;
+pub(crate) const P_GROW_LEAF: f64 = 0.1;
+
+// Evaluation
+pub(crate) const MAX_DEPTH: usize = 12;
+pub(crate) const MAX_PROGRAM_RUNTIME_MS: u128 = 10;
+/// The bigger, the less important tree depth is in individual evaluation
+pub(crate) const DEPTH_SOFTENER: f64 = 6.0;
+pub(crate) const SCORE_PROGRAM_ERROR: f64 = f64::NEG_INFINITY;
+pub(crate) const SCORE_DEPTH_LIMIT_EXCEEDED: f64 = f64::NEG_INFINITY;
+
+// Mutation
 pub(crate) const MUTATION_CHANCE: f64 = 0.05;
 pub(crate) const MUTATION_SINGLE_CHANCE: f64 = 0.10;
 
-pub(crate) const P_GROW_LEAF: f64 = 0.1;
-
+// Crossover
+pub(crate) const CROSSOVER_SIZE: usize = (POPULATION_SIZE as f64 * 0.2) as usize;
