3662368c4863b0cb54cd6424b4e04bd6a4694978
limelier <tudor.iacobescu.1@gmail.com>
2021-06-16 15:42:51 +0300

    Add first example.

--- result_crawler.py
+++ result_crawler.py
@@ -1,3 +1,5 @@
+import sys
+
 import parse
 from numpy import arange
 
@@ -16,7 +18,7 @@ class Gen:
 
 
 ranges = {
-    'individuals_per_method_and_depth': Gen(range(2, 31, 2), 10, False),
+    'individuals_per_method_and_depth': Gen(range(2, 31, 2), 5, False),
     'generations': Gen(range(100, 5000, 100), 1000, False),
     'p_grow_leaf': Gen(arange(0.0, 0.6, .05), 0.1, False),
     'min_gen_depth': Gen(range(1, 6, 1), 2, False),
@@ -24,10 +26,12 @@ ranges = {
     'depth_softener': Gen([1.0, 6.0, 10.0], 6.0, True),
     'max_program_runtime_ms': Gen(range(5, 25, 5), 10, False),
     'max_depth': Gen(range(5, 15, 1), 12, False),
-    'mutation_chance': Gen([0.0, 0.02, 0.05, 0.1, 0.5], 0.05, True),
-    'mutation_single_chance': Gen([0.0, 0.05, 0.1, 0.3, 0.7], 0.1, True),
-    'crossover_size_proportion': Gen([0.2, 0.5, 0.75], 0.75, True),
+    'mutation_chance': Gen([0.0, 0.001, 0.01, 0.02, 0.05], 0.05, True),
+    'mutation_single_chance': Gen([0.0, 0.001, 0.01, 0.05, 0.1], 0.1, True),
+    'crossover_size_proportion': Gen([0.5, 0.75, 0.9], 0.75, True),
     'selection_pressure': Gen([0.2, 1.0, 5.0], 1.0, True),
+    'tournament_size': Gen([2, 4, 7], 4, True),
+    'tournament_p': Gen([0.8, 1.0], 1.0, True),
 }
 
 
@@ -39,13 +43,15 @@ format_string = \
     '{:d}/{:d} ({:d}, {:d}, {:f}, {:d}, {:d}, {:f}, {:d}, {:d}, {:f}, {:f}, {:f}, {:f}): ({:f}, {:f}) in {:f}s\n'
 parsed_lines = []
 
-with open('search_2.txt', 'r') as f:
+with open(sys.argv[1], 'r') as f:
     for line in f:
         parsed = parse.parse(format_string, line)
         if parsed is not None:
             match = parsed[2:]
             values = dict(zip(ranges.keys(), match[:12]))
             result = match[12:]
+            if len(result) < 3:
+                continue
             parsed_lines.append((values, result))
 
 print('Average dice index, depth and time for:\n')
@@ -54,6 +60,8 @@ for key, vals in ranges.items():
     for value in vals.items():
         print(f' {value:>7}: ', end='')
         lines = filter(lambda parsed_line: parsed_line[0][key] == value, parsed_lines)
-        results = (line[1] for line in lines)
+        results = list(line[1] for line in lines)
+        if len(results) == 0:
+            continue
         scores, depths, times = zip(*results)
         print(f'{avg(scores):.4f}, {avg(depths):.4f}, {avg(times):.4f}')
