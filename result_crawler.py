ea3d3ffc235989993d6c8cbb45024ee35a731aca
limelier <tudor.iacobescu.1@gmail.com>
2021-06-18 16:38:45 +0300

    Add analysis.

--- result_crawler.py
+++ result_crawler.py
@@ -29,7 +29,7 @@ ranges = {
     'mutation_chance': Gen([0.0, 0.001, 0.01, 0.02, 0.05], 0.05, True),
     'mutation_single_chance': Gen([0.0, 0.001, 0.01, 0.05, 0.1], 0.1, True),
     'crossover_size_proportion': Gen([0.5, 0.75, 0.9], 0.75, True),
-    'selection_pressure': Gen([0.2, 1.0, 5.0], 1.0, True),
+    'selection_pressure': Gen([0.2, 1.0, 5.0], 1.0, False),
     'tournament_size': Gen([2, 4, 7], 4, True),
     'tournament_p': Gen([0.8, 1.0], 1.0, True),
 }
@@ -40,28 +40,35 @@ def avg(lst):
 
 
 format_string = \
-    '{:d}/{:d} ({:d}, {:d}, {:f}, {:d}, {:d}, {:f}, {:d}, {:d}, {:f}, {:f}, {:f}, {:f}): ({:f}, {:f}) in {:f}s\n'
+    '{:d}/{:d} ' \
+    '({:d}, {:d}, {:f}, {:d}, {:d}, {:f}, {:d}, ' \
+    '{:d}, {:f}, {:f}, {:f}, {:f}, {:d}, {:f}): ' \
+    '({:f}, {:f}) in {:f}s\n'
 parsed_lines = []
 
 with open(sys.argv[1], 'r') as f:
     for line in f:
+        if not line[0].isdigit():
+            continue
         parsed = parse.parse(format_string, line)
         if parsed is not None:
             match = parsed[2:]
-            values = dict(zip(ranges.keys(), match[:12]))
-            result = match[12:]
+            values = dict(zip(ranges.keys(), match[:14]))
+            result = match[14:]
             if len(result) < 3:
                 continue
             parsed_lines.append((values, result))
 
-print('Average dice index, depth and time for:\n')
+print('Average dice index and depth for each parameter\'s value:\n')
 for key, vals in ranges.items():
+    if not vals.change:
+        continue
     print(key)
     for value in vals.items():
-        print(f' {value:>7}: ', end='')
         lines = filter(lambda parsed_line: parsed_line[0][key] == value, parsed_lines)
         results = list(line[1] for line in lines)
-        if len(results) == 0:
+        if len(results) < 3:
             continue
+        print(f' {value:>7}: ', end='')
         scores, depths, times = zip(*results)
-        print(f'{avg(scores):.4f}, {avg(depths):.4f}, {avg(times):.4f}')
+        print(f'{avg(scores):.4f}, {avg(depths):.4f}')
